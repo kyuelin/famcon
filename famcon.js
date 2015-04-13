43b77a294bfd9d5d30cd5adb13c65c33b6ad626f
kyuelin <kyuelin@gmail.com>
2015-04-13 02:18:12 -0400

    updated with geoloc

--- famcon.js
+++ famcon.js
@@ -1,9 +1,40 @@
+Coords = new Mongo.Collection("coords");
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    coords: [
-      { pos : "(Latitude, Longitude, Accuracy, Timestamp) = (40.7619316,-73.7889575,477,1428814097904)" }
-    ]
+    coords: function() {
+      return Coords.find({});
+    }
   });
+
+  Template.body.events({
+    "click .butt1": function (event) {
+      function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition, noPosition);
+        }
+      }
+      function showPosition(position) {
+        var pos = "(Latitude, Longitude, Accuracy, Timestamp) = ("
+            + position.coords.latitude + ","
+            + position.coords.longitude + ","
+            + position.coords.accuracy + ","
+            + position.timestamp + ")";
+        Coords.insert(
+          //pos: pos,
+          //createdAt: new Date(),
+          //co: position.coords
+          position.coords
+          //position
+        );
+      }
+      function noPosition(err) {
+        console.warn('ERROR(' + err.code + '): ' + err.message);
+      }
+      getLocation();
+      return false;
+    }
+  })
 }
 
 if (Meteor.isServer) {
@@ -11,3 +42,5 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+
